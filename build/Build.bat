3c59db62e8eda7bd1714961a6803a806704ef0c3
jiniannet <hnvvv@163.com>
2014-07-29 17:18:09 +0800

    update

---
+++ build/Build.bat
@@ -0,0 +1,12 @@
+@echo off
+echo 开始生成JNTemplate 2.0 ...
+set fdir=%WINDIR%\Microsoft.NET\Framework
+cd ../src/JinianNet.JNTemplate
+echo v2.0.50727
+%fdir%\v3.5\csc.exe /target:library /out:../../lib/2.0/JinianNet.JNTemplate.dll /doc:../../lib/2.0/JinianNet.JNTemplate.xml /keyfile:../../tool/jiniannet.snk /warn:3 /nologo /o /recurse:*.cs
+%fdir%\v4.0.30319\csc.exe /target:library /out:../../lib/4.0/JinianNet.JNTemplate.dll /doc:../../lib/4.0/JinianNet.JNTemplate.xml /keyfile:../../tool/jiniannet.snk /warn:3 /nologo /o /recurse:*.cs
+cd ..
+cd ..
+cd build
+echo 生成完毕...
+pause
