557a175aaa7d9bb2c5b91c8eec1eaba649d8c22d
jiniannet <hnvvv@163.com>
2014-07-31 17:57:39 +0800

    BUILD

--- build/Build.bat
+++ build/Build.bat
@@ -2,9 +2,10 @@
 echo 开始生成 JNTemplate ...
 set fdir=%WINDIR%\Microsoft.NET\Framework
 cd ../src/JinianNet.JNTemplate
-echo v2.0.50727
 %fdir%\v3.5\csc.exe /target:library /out:../../lib/2.0/JinianNet.JNTemplate.dll /doc:../../lib/2.0/JinianNet.JNTemplate.xml /keyfile:../../tool/jiniannet.snk /warn:3 /nologo /o /recurse:*.cs
+echo v2.0.50727 生成完毕
 %fdir%\v4.0.30319\csc.exe /target:library /out:../../lib/4.0/JinianNet.JNTemplate.dll /doc:../../lib/4.0/JinianNet.JNTemplate.xml /keyfile:../../tool/jiniannet.snk /warn:3 /nologo /o /recurse:*.cs
+echo v4.0.30319 生成完毕
 cd ..
 cd ..
 cd build
