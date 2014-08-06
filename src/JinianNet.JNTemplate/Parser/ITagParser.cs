cf9d9abcd402f373b5b164dd697dfc1263b1c8ec
jiniannet <hnvvv@163.com>
2014-08-06 09:51:13 +0800

    修复一个FOR标签错误

--- src/JinianNet.JNTemplate/Parser/ITagParser.cs
+++ src/JinianNet.JNTemplate/Parser/ITagParser.cs
@@ -241,7 +241,7 @@ namespace JinianNet.JNTemplate.Parser
         public Tag Parse(TemplateParser parser, TokenCollection tc)
         {
 
-            if (tc.Count > 3 && Common.ParserHelpers.IsEqual(Field.KEY_FOREACH, tc.First.Text))
+            if (tc.Count > 3 && Common.ParserHelpers.IsEqual(Field.KEY_FOR, tc.First.Text))
             {
 
                 if (tc[1].TokenKind == TokenKind.LeftParentheses
--- src/JinianNet.JNTemplate/Parser/Node/IfTag.cs
+++ src/JinianNet.JNTemplate/Parser/Node/IfTag.cs
@@ -1,4 +1,4 @@
-﻿//*****************************************************
+﻿/*****************************************************
    Copyright (c) 2013-2014 翅膀的初衷  (http://www.jiniannet.com)
 
    Licensed under the Apache License, Version 2.0 (the "License");
