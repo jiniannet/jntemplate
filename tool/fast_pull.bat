1dca0ea8c9946b8d0377bc319b804e1bc4eacb77
jiniannet <hnvvv@163.com>
2017-03-01 17:28:35 +0800

    update tools

---
+++ tool/fast_pull.bat
@@ -0,0 +1,3 @@
+git push --progress "https://git.oschina.net/jiniannet/jntemplate.git" refs/heads/dev:refs/heads/master
+git push --progress "https://git01.codeplex.com/jntemplate" refs/heads/dev:refs/heads/master
+git push --progress "http://code.jiniannet.com/git/jntemplate.git" refs/heads/dev:refs/heads/master
