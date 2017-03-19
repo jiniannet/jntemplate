f5876210982e2bda0a0d3529c989ff4a83442109
翅膀的初衷 <hnvvv@163.com>
2017-03-19 16:14:39 +0800

    ci conf

---
+++ appveyor_core_conf.bat
@@ -0,0 +1,4 @@
+@echo off
+ren src\JinianNet.JNTemplate\Properties\AssemblyInfo.cs AssemblyInfo.cs.build.tmp
+ren src\JinianNet.JNTemplate.Test\Properties\AssemblyInfo.cs AssemblyInfo.cs.build.tmp
+echo ren success.
