0d7538a7f25af0daefb504f3242a6d1da2a84cc4
翅膀的初衷 <hnvvv@163.com>
2017-03-20 21:29:53 +0800

    appveyor

--- appveyor_core_conf.bat
+++ appveyor_core_conf.bat
@@ -2,3 +2,5 @@
 ren src\JinianNet.JNTemplate\Properties\AssemblyInfo.cs AssemblyInfo.cs.build.tmp
 ren src\JinianNet.JNTemplate.Test\Properties\AssemblyInfo.cs AssemblyInfo.cs.build.tmp
 echo ren success.
+nuget restore JinianNet.JNTemplate_core.sln
+echo ren success.
