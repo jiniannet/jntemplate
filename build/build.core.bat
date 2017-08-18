e9ad34d259d127b3e1765d9859bf455786e8f6fd
jiniannet <hnvvv@163.com>
2017-08-18 17:43:21 +0800

    更新.NET CORE版本为2.0

--- build/build.core.bat
+++ build/build.core.bat
@@ -9,6 +9,7 @@ if not exist ..\..\lib\netcoreapp1.1 md ..\..\lib\netcoreapp1.1
 ren .\Properties\AssemblyInfo.cs AssemblyInfo.cs.build.tmp
 echo ¿ªÊ¼build
 dotnet build JinianNet.JNTemplate_core.csproj --configuration Release --output ..\..\lib\netcoreapp1.1 --framework netcoreapp1.1 
+dotnet build JinianNet.JNTemplate_core.csproj --configuration Release --output ..\..\lib\netcoreapp2.0 --framework netcoreapp2.0 
 ren .\Properties\AssemblyInfo.cs.build.tmp AssemblyInfo.cs
 cd ..
 cd ..
