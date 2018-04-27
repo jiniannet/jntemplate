3eec6d2f22b842b93f69bea18efb7ca107ddfd7b
jiniannet <hnvvv@163.com>
2018-04-27 17:13:32 +0800

    CODEING

---
+++ src/JinianNet.JNTemplate/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿/********************************************************************************
+ Copyright (c) jiniannet (http://www.jiniannet.com). All rights reserved.
+ Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+ ********************************************************************************/
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("JNTemplate")]
+[assembly: AssemblyDescription("Open source templating engine for .net/mono")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JinianNet.com")]
+[assembly: AssemblyProduct("JNTemplate")]
+[assembly: AssemblyCopyright("Copyright © JinianNet.com")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(true)]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("de260545-c1e6-4349-bfd2-da6ce96393dd")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0")]
