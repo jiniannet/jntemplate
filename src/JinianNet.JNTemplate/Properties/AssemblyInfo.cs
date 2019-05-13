764a69c0a27f9e49264875fe287ab8fc16811604
翅膀的初衷 <hnvvv@163.com>
2019-05-13 09:31:01 +0800

    V1.4.0.1

--- src/JinianNet.JNTemplate/Properties/AssemblyInfo.cs
+++ src/JinianNet.JNTemplate/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(true)]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("de260545-c1e6-4349-bfd2-da6ce96393dd")]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0")]
+[assembly: AssemblyVersion("1.4.0.1")]
+[assembly: AssemblyFileVersion("1.4.1")]
