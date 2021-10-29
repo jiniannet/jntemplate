0fb21dfaa2ed43e4db7c50033d0e59203f6eb697
翅膀的初衷 <hnvvv@163.com>
2021-10-29 20:43:39 +0800

    修复StringReader

--- src/JinianNet.JNTemplate/Const.cs
+++ src/JinianNet.JNTemplate/Const.cs
@@ -15,7 +15,7 @@ namespace JinianNet.JNTemplate
         /// <summary>
         /// Version
         /// </summary>
-        public const string Version = "2.2.2";
+        public const string Version = "2.2.4";
         internal const string KEY_FOREACH = "foreach";
         internal const string KEY_IF = "if";
         internal const string KEY_ELSEIF = "elseif";
--- src/JinianNet.JNTemplate/Resources/StringReader.cs
+++ src/JinianNet.JNTemplate/Resources/StringReader.cs
@@ -3,8 +3,6 @@
  Licensed under the MIT license. See licence.txt file in the project root for full license information.
  ********************************************************************************/
 using System;
-using System.IO;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace JinianNet.JNTemplate.Resources
@@ -12,16 +10,31 @@ namespace JinianNet.JNTemplate.Resources
     /// <summary>
     /// 
     /// </summary>
-    public class StringReader : System.IO.StringReader, IReader
+    public class StringReader : /*System.IO.StringReader,*/ IReader
     {
+        private string content;
         /// <summary>
         /// 
         /// </summary>
         /// <param name="text"></param>
         public StringReader(string text)
-            :base(text)
+        //:base(text)
         {
-
+            content = text;
         }
+
+
+        /// <inheritdoc />
+        public string ReadToEnd()
+        {
+            return content;
+        }
+#if !NF40
+        /// <inheritdoc />
+        public Task<string> ReadToEndAsync()
+        {
+            return Task.FromResult(content);
+        }
+#endif
     }
 }
